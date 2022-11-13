186adbdd650b791f52ee98a076e8515402e3e78a
teodanciu <teodora.danciu@tweag.io>
2022-11-13 22:17:36 +0000

    Always do logic for restrictPoolMetadataHash

--- eras/shelley/impl/src/Cardano/Ledger/Shelley/SoftForks.hs
+++ eras/shelley/impl/src/Cardano/Ledger/Shelley/SoftForks.hs
@@ -21,4 +21,4 @@ restrictPoolMetadataHash ::
   (HasField "_protocolVersion" pp ProtVer) =>
   pp ->
   Bool
-restrictPoolMetadataHash pp = getField @"_protocolVersion" pp > ProtVer 4 0
+restrictPoolMetadataHash _ = True
